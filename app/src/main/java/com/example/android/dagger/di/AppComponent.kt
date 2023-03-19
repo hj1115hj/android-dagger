c3d8d186318b7d87d401a7802ecfe1b537b72ea8
hj1115hj <hyejin_one@naver.com>
2023-03-19 18:11:11 +0900

    feat: 하위 구성 요소

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -3,6 +3,7 @@ package com.example.android.dagger.di
 import android.content.Context
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
+import com.example.android.dagger.registration.RegistrationComponent
 import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
 import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
 import dagger.BindsInstance
@@ -10,7 +11,7 @@ import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [StorageModule::class])
+@Component(modules = [StorageModule::class, AppSubcomponents::class])
 interface AppComponent {
     @Component.Factory
     interface Factory{
@@ -19,6 +20,5 @@ interface AppComponent {
     fun inject(activity: RegistrationActivity)
     fun inject(activity: MainActivity)
 
-    fun inject(fragment: EnterDetailsFragment)
-    fun inject(fragment: TermsAndConditionsFragment)
+    fun registrationComponent(): RegistrationComponent.Factory
 }
