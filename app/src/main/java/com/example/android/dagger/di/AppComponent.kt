446a15a52174d70ae0b4984a7b121e4155f09383
hj1115hj <hyejin_one@naver.com>
2023-03-19 18:49:14 +0900

    feat: 로그인 흐름 리팩터링

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -1,6 +1,7 @@
 package com.example.android.dagger.di
 
 import android.content.Context
+import com.example.android.dagger.login.LoginComponent
 import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
 import com.example.android.dagger.registration.RegistrationComponent
@@ -20,4 +21,6 @@ interface AppComponent {
     fun inject(activity: MainActivity)
 
     fun registrationComponent(): RegistrationComponent.Factory
+
+    fun loginComponent(): LoginComponent.Factory
 }
